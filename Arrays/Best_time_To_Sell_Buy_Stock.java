ff3a9d1027d3b4b261c2667266ec068d3774b41a
Sannidhya <Sannidhyavyas4@gmail.com>
2026-08-08 02:22:07 +0530

    Solve Stock Problem

---
+++ Arrays/Best_time_To_Sell_Buy_Stock.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class Best_time_To_Sell_Buy_Stock {
+    public static int stock(int prices[]){
+        int buyprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+
+        for(int i=0; i<prices.length; i++){
+            if(buyprice<prices[i]){
+                int profit = prices[i]- buyprice;  // todays profit
+
+                maxprofit= Math.max(profit, maxprofit);
+            }
+            else {
+                buyprice= prices[i];
+            }
+        }
+        return maxprofit;
+
+    }
+
+    public static void main(String[] args){
+        int prices[]= {7,1,5,3,6,4};
+
+        System.out.println(stock(prices));
+    }
+}
