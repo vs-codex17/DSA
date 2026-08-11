5eb1981cd08043f2e34f65a1b4b31ac8350fbed8
vs-codex17 <sannidhya06vyas@gmail.com>
2026-08-12 00:01:41 +0530

    Solve diagonal sum problem

---
+++ Arrays/Array_2D/Diagonal_Sum.java
@@ -0,0 +1,46 @@
+package Arrays.Array_2D;
+
+public class Diagonal_Sum {
+    public static void Sum(int matrix[][]){
+        int sum=0;
+
+//        for(int i=0; i< matrix.length; i++){
+//            for(int j=0; j<matrix[0].length; j++){
+//                if(i==j){
+//                    sum+=matrix[i][j];
+//                }
+//                else if (i+j==matrix.length-1) {
+//                    sum+=matrix[i][j];
+//                }
+//            }
+//        }
+
+
+        //Method 2
+
+        for(int i=0; i< matrix.length; i++){
+            //PD
+            sum+=matrix[i][i];
+
+            //SD
+            if(i!= matrix.length-1-i){
+                sum+=matrix[i][matrix.length-1-i];
+            }
+        }
+        System.out.println("Total Sum = "+sum);
+
+    }
+
+    public static void main(String[] args){
+//        int matrix[][]= {{1,2,3,4},
+//                        {5,6,7,8},
+//                        {9,10,11,12},
+//                        {13,14,15,16}};
+
+        int matrix[][]= {{1,2,3},
+                        {5,6,7},
+                        {9,10,11}};
+
+        Sum(matrix);
+    }
+}
